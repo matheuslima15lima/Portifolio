8dc6da9a273fb09afd0a08b574655feb89e1a7f3
Matheus Lima <limalmatheus7@gmail.com>
2025-02-12 18:05:32 -0300

    feita funcionalidade de menu

--- matheus-lima/src/Components/Header/Header.jsx
+++ matheus-lima/src/Components/Header/Header.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import { Subtitle} from '../Title/Title'
+import { IoMenu } from "react-icons/io5";
 export const Header = () => {
-  return (
-    <div className='nav-bar'>
+  const handleClick = () => {
+alert('Você clicou no X!');
+};
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const toggleMenu= ()=> {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  return (
+    <header className='header'>
     
-      {/* <button className='close-button'>x</button> */}
-      <span style={{ color: 'red', fontSize: '24px' }}>x</span>
+
+    <IoMenu className='menu-icon' style={{color: 'white', width:'24px', height:'24px' , cursor: 'pointer'}}
+      onClick={toggleMenu}
+    />
+
+    <div className={`nav-bar ${isMenuOpen ? 'open' : ''}`}>
+
+    {isMenuOpen && (
+  <span className='xspan' onClick={toggleMenu}>x</span>
+)}
+    
   <ul>
     <li><a href="#about" className='textHeader'>Sobre</a></li>
     <li><a href="#services"className='textHeader'>Habilidades</a></li>
     <li><a href="#contact"className='textHeader'>Projetos</a></li>
   </ul>
-
     </div>
+
+    </header>
   )
 }
