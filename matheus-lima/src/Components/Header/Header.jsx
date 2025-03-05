42619aa946eafeb78c7c9ef82de5360acba898d0
Matheus Lima <limalmatheus7@gmail.com>
2025-03-05 18:24:10 -0300

    fazendo menu

--- matheus-lima/src/Components/Header/Header.jsx
+++ matheus-lima/src/Components/Header/Header.jsx
@@ -1,41 +1,44 @@
 import React, { useState } from 'react'
 import './Header.css'
-import { Subtitle} from '../Title/Title'
+import { Subtitle } from '../Title/Title'
 import { IoMenu } from "react-icons/io5";
 export const Header = () => {
   const handleClick = () => {
-alert('Você clicou no X!');
-};
+    alert('Você clicou no X!');
+  };
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-  const toggleMenu= ()=> {
+
+  const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   }
   return (
     <header className='header'>
-    
 
-    <IoMenu className='menu-icon' style={{color: 'white', width:'40px', height:'40px' , cursor: 'pointer'}}
+
+      <IoMenu className='menu-icon' style={{ color: 'white', width: '40px', height: '40px', cursor: 'pointer' }}
+        onClick={toggleMenu}
+      />
+
+      <div className= "nav-bar" >
+
+
+          <IoMenu className='menu-icon' style={{ color: 'white', width: '24px', height: '24px', cursor: 'pointer', marginLeft:'50px'}}
       onClick={toggleMenu}
     />
 
-    <div className={`nav-bar ${isMenuOpen ? 'open' : ''}`}>
+        <div className={`div-content${isMenuOpen ? 'open' : ''}`}>
+          {/* {isMenuOpen?(
+) : null} */}
+          {/* <span className='xspan' onClick={toggleMenu}>x</span> */}
+          <ul className={`ul${isMenuOpen ? 'open' : ''}`}>
+            <li><a href="#about" className='textHeader'>Sobre</a></li>
+            <li><a href="#services" className='textHeader'>Habilidades</a></li>
+            <li><a href="#contact" className='textHeader'>Projetos</a></li>
+          </ul>
+        </div>
 
-    {isMenuOpen?(
-  <span className='xspan' onClick={toggleMenu}>x</span>
-) : null}
-    
-   <div className='div-content'>
-    
-  <ul>
-    <li><a href="#about" className='textHeader'>Sobre</a></li>
-    <li><a href="#services"className='textHeader'>Habilidades</a></li>
-    <li><a href="#contact"className='textHeader'>Projetos</a></li>
-  </ul>
-    </div> 
-
-    </div>
+      </div>
 
     </header>
   )
